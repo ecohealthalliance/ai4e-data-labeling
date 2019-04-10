dff35aaca3bd36c974b651c87464c8743f5b15d7
Toph Allen <toph.allen@gmail.com>
2019-04-10 18:13:34 -0400

    Minor fix

--- dump_articles.sh
+++ dump_articles.sh
@@ -6,4 +6,4 @@ terms="[$terms]"
 query="{ \"text_matches\": { \"\$in\": $terms } }\""
 echo $query
 
-mongodump -d pmc -c articles -q '{ "text_matches": { "$in": ["field work","fieldwork","field study","field site","field area","study site","study location","study area","research site","research location","research area","sampling site","sampling location","sampling area"] } }' --archive "dump.zip"
+mongodump --gzip --archive=ai4e_articles.gzip -d pmc -c articles -q '{ "text_matches": { "$in": ["field work","fieldwork","field study","field site","field area","study site","study location","study area","research site","research location","research area","sampling site","sampling location","sampling area"] } }'
